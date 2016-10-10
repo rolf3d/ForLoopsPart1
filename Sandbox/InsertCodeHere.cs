afaa50f5ee237a08cf2d4498c36955fa785b3c70
Martin Kierkegaard <mark@easj.dk>
2016-10-10 23:09:48 +0200

    solution for the exercise

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -13,7 +13,7 @@ namespace Sandbox
             Console.WriteLine("Start of case 1");
             for (int i = 0; i < 15; i = i + 3)
             {
-                // Console.WriteLine(i); // Uncomment to see result
+                Console.WriteLine(i); // Uncomment to see result
             }
             Console.WriteLine("End of case 1");
 
@@ -22,29 +22,38 @@ namespace Sandbox
             Console.WriteLine("Start of case 2");
             for (int i = 10; i >= 3; i--)
             {
-                // Console.WriteLine(i); // Uncomment to see result
+                Console.WriteLine(i); // Uncomment to see result
             }
             Console.WriteLine("End of case 2");
 
 
             // Case 3 - Change this to a for-loop!
+            // int c3 = 0;
+            // while (c3 < 12)
+            // {
+            //     c3 = c3 + 2;
+            //     Console.WriteLine(c3);
+            // }
             Console.WriteLine("Start of case 3");
-            int c3 = 0;
-            while (c3 < 12)
+            for (int i = 0; i < 12; i = i + 2)
             {
-                c3 = c3 + 2;
-                Console.WriteLine(c3);
+                Console.WriteLine(i + 2);
             }
             Console.WriteLine("End of case 3");
 
 
-            // Case 4 - Change this to a for-loop!
+            // Case 4 - Change this to a for-loop!          
+            // int c4 = 0;
+            // while (c4 < 50)
+            // {
+            //     Console.WriteLine(c4);
+            //     c4 = c4 * 2 + 2;
+            // }
+
             Console.WriteLine("Start of case 4");
-            int c4 = 0;
-            while (c4 < 50)
+            for (int i = 0; i < 50; i = i * 2 + 2)
             {
-                Console.WriteLine(c4);
-                c4 = c4 * 2 + 2;
+                Console.WriteLine(i);
             }
             Console.WriteLine("End of case 4");
 
