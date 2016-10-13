962d8aeefed70a99fb34015f50ac77dd0761c599
Rolf Petersen <rollep7@msn.com>
2016-10-13 12:59:35 +0200

    3 og 4 ændret til for loop

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -31,26 +31,34 @@ namespace Sandbox
 
             // Case 3 - Change this to a for-loop!
             Console.WriteLine("Start of case 3");
-            int c3 = 0;
-            while (c3 < 12)
-            {
-                c3 = c3 + 2;
-                // 2-4-6-8-10-12
-                //Console.WriteLine(c3);
-            }
+            //int c3 = 0;
+            //while (c3 < 12)
+            //{
+            //    c3 = c3 + 2;
+            //    // 2-4-6-8-10-12
+            //    //Console.WriteLine(c3);
+            //}
             Console.WriteLine("End of case 3");
+            for (int c3 = 2; c3 <= 12; c3 = c3 + 2)
+            {
+                Console.WriteLine(c3);
+            }
 
 
             // Case 4 - Change this to a for-loop!
             Console.WriteLine("Start of case 4");
-            int c4 = 0;
-            while (c4 < 50)
+            //int c4 = 0;
+            //while (c4 < 50)
+            //{
+            //    Console.WriteLine(c4);
+            //    // 0-2-6-14-30
+            //    c4 = c4 * 2 + 2;
+            //}
+            Console.WriteLine("End of case 4");
+            for (int c4 = 0; c4 <= 50; c4 = c4 * 2 + 2)
             {
                 Console.WriteLine(c4);
-                // 0-2-6-14-30
-                c4 = c4 * 2 + 2;
             }
-            Console.WriteLine("End of case 4");
 
             // The LAST line of code should be ABOVE this line
         }
